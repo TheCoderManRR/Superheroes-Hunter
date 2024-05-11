f3d160107644dfd3d283cda705a9648d9cdd9b6b
TheCoderManRR <167234351+TheCoderManRR@users.noreply.github.com>
2024-05-11 13:36:39 +0530

    Add files via upload

---
+++ INDEX/superhero.js
@@ -0,0 +1,23 @@
+const params = new URLSearchParams(window.location.search);
+const superheroId = params.get('id');
+
+const getSuperheroDetails = async () => {
+    const url = `http://gateway.marvel.com/v1/public/characters/${superheroId}?ts=1&apikey=b8ffec201008949c2422b84410b608ac&hash=2612f31a32ed1f9e9146bce6cfcca5b5`;
+    const response = await fetch(url);
+    const jsonData = await response.json();
+    const superhero = jsonData.data.results[0];
+    const comicsAvailable = superhero.comics.available;
+    const seriesAvailable = superhero.series.available;
+    const storiesAvailable = superhero.stories.available;
+
+
+    document.getElementById('superhero-name').textContent = superhero.name;
+    document.getElementById('comics').textContent = `Total Number of Comics: ${comicsAvailable}`;
+    document.getElementById('series').textContent = `Total Number of Series: ${seriesAvailable}`;
+    document.getElementById('stories').textContent = `Total Number of Stories: ${storiesAvailable}`;
+    document.getElementById('superhero-description').textContent = superhero.description || 'N/A';
+    document.getElementById('superhero-image').src = `${superhero.thumbnail.path}/portrait_fantastic.${superhero.thumbnail.extension}`;
+
+};
+
+window.onload = getSuperheroDetails;
